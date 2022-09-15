0c6824b2c2417ebd17ecf68c8ab2e24e39ed55ab
ElBetoso <108848743+ElBetoso@users.noreply.github.com>
2022-09-15 17:44:36 -0500

    Update cart_controller_test.rb

--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CartControllerTest < ActionDispatch::IntegrationTest
 test "should get show" do
-    get cart_show_url
+    get cart_url
     assert_response :success
   end
 end
