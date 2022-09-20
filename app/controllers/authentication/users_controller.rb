165418df49dd2977f356d52495ad0172dda4648d
ElBetoso <108848743+ElBetoso@users.noreply.github.com>
2022-09-20 12:40:47 -0500

    Update users_controller.rb

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -1,14 +1,14 @@
 class Authentication::UsersController < ApplicationController
 skip_before_action :protect_pages
   def new
-    @user = User.new
+    @users = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @users = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
+    if @users.save
+      session[:user_id] = @users.id
       redirect_to restaurants_path, notice: "created"
     else
       render :new, status: :unprocessable_entity
