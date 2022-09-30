d629559fc4b7f7845a0e0588c6457c8a0d9a038e
ElBetoso <108848743+ElBetoso@users.noreply.github.com>
2022-09-29 19:31:29 -0500

    Update 20220815173509_add_dish_groupsto_dishes.rb

--- db/migrate/20220815173509_add_dish_groupsto_dishes.rb
+++ db/migrate/20220815173509_add_dish_groupsto_dishes.rb
@@ -1,5 +1,5 @@
 class AddDishGroupstoDishes < ActiveRecord::Migration[7.0]
   def change
-      add_reference :dishes, :dish_group, null: false, 
+      add_reference :dishes, :dish_group, null: false 
   end
 end
